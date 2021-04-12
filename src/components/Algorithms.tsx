f39123be1ce7e9aadb8980bbec730fcdf4bab630
Jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2021-04-12 21:47:12 +0200

    fix: made mergesort typescrip

--- src/components/Algorithms.tsx
+++ src/components/Algorithms.tsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { mergeSort } from '../algorithms/mergeSort'
 
 const Algorithms = () => {
 
-  const values = [3, 4]
+  const [values, setValues] = useState([3, 4, 1, 5, 2, 9])
+
+  const merge = () => {
+    const newValues = mergeSort(values, 0, values.length)
+    setValues(newValues)
+  }
+
 
   return (
-    <div className="algorithms">
-      {
-        values.map((value, index) => (
-          <h3 className="algorithm-value" key={index}> value </h3>
-        ))
-      }
-    </div>
+    <>
+      <div className="algorithms">
+        {
+          values.map((value, index) => (
+            <div className="algorithm-value" key={index} style={{height: `${(value * 10)}%`}}> {value} </div>
+          ))
+        }
+      </div>
+      <button className="run-algorithm" onClick={merge}> Merge </button>
+    </>
   )
 }
 
