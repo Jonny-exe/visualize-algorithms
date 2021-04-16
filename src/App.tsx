f46b2d24dab3ce69caa93f53cc020b5f23301439
Jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2021-04-16 21:39:58 +0200

    feat: added template for showing source code

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import AlgorithmSelection from './components/AlgorithmSelection'
 import Algorithms from './components/Algorithms'
 
 function App() {
+
+  const [showCode, setShowCode] = useState(false)
+  const toggleCode = () => {
+    setShowCode(!showCode)
+  }
+
   return (
     <div className="App">
       <AlgorithmSelection />
         <div className="algorithm-container">
           <Algorithms />
-          <button class="see-code">{"<>"}</button>
         </div>
+        <button onClick={toggleCode} class="see-code">{"<>"}</button>
+          <div className={`code ${showCode ? "active" : ""}`}>
+            <code>
+              Hello this is the code
+            </code>
+          </div>
+          <div onClick={toggleCode} className={`code-background ${showCode ? "active" : ""}`}>
+          </div>
     </div>
   );
 }
