4140eeefbda59e8797097fec322cf9fc04a15f5a
Jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2021-04-17 11:19:11 +0200

    feat: added functioning algorithm selector

--- src/components/AlgorithmSelection.tsx
+++ src/components/AlgorithmSelection.tsx
@@ -1,13 +1,52 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const AlgorithmSelection = () => {
+interface Props {
+  algorithmType: string;
+  setAlgorithmType: (algo_type: string) => void;
+}
+
+const AlgorithmSelection: React.FC<Props> = ({algorithmType, setAlgorithmType}) => {
+
+  const [showSelectionWindow, setShowSelectionWindow] = useState(false)
+  const getPrettiAlgoritmName = (algorithmType: string) => {
+    let result = ""
+    switch (algorithmType) {
+      case "quicksort":
+        result = "Quick sort"
+        break
+      case "mergesort":
+        result = "Merge sort"
+        break
+    }
+    return result
+  }
+
+  const changeAlgorithmType = (newAlgorithmType: string) => {
+    setAlgorithmType(newAlgorithmType)
+    setShowSelectionWindow(false)
+  }
+
+
+  const prettiAlgoritmName = getPrettiAlgoritmName(algorithmType)
+
   return (
-    <div className="algorithm-selection"> 
-      <h2> Merge sort </h2>
+    <>
+    <div className="algorithm-selection"  onClick={() => setShowSelectionWindow(true)}> 
+      <h2> {prettiAlgoritmName} </h2>
       <hr/>
     </div>
+    <div className={`algorithm-selection-window ${showSelectionWindow ? "active" : ""}`}>
+      <div>
+      <h2 style={{width: 'auto', height: "auto"}}> Choose an algorithm </h2>
+      <hr/>
+      </div>
+      <button onClick={() => changeAlgorithmType("mergesort")} className="window-button">Merge sort</button>
+      <button onClick={() => changeAlgorithmType("quicksort")} className="window-button">Quick sort</button>
+    </div>
+    <div className={`algorithm-selection-window-background ${showSelectionWindow ? "active" : ""}`} onClick={() => setShowSelectionWindow(false)}>
+    </div>
+    </>
   )
-
 }
 
 export default AlgorithmSelection
